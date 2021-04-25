827b45f743cff97d06ad739a30888c98e1f61262
angelinas8744 <73479626+angelinas8744@users.noreply.github.com>
2021-04-25 17:30:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
 });
 
-document.querySelector("#github").href = "http://github.com/" + userName;
+document.querySelector("#github").href = "https://github.com/angelinas8744" + userName;
 document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
